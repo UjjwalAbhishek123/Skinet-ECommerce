4ddf864764b334122f7a8c65709fc7c9f9408509
UjjwalAbhishek123 <ujjwalabhishek15@gmail.com>
2025-01-10 14:17:54 +0530

    Added json Seed Data in Db Table Products through StoreContextSeed.cs and automated DB migration in Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,7 @@
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Validations;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -31,5 +32,30 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapControllers();
+
+//we want to automate the migrations whenever the application starts
+//and make data up-to-date
+try
+{
+    // Create a scope(temp container by which we can use Services through DI) to access application services like DbContext
+    using var scope = app.Services.CreateScope();
+
+    // Get the service provider(place where all dependencies get registered) from the scope
+    var services = scope.ServiceProvider;
+
+    // Get the StoreContext (our DbContext) through DI to interact with the database
+    var context = services.GetRequiredService<StoreContext>();
+
+    // Apply any pending migrations to the database (auto-update schema)
+    await context.Database.MigrateAsync();
+
+    // Seed initial data to the database if it’s empty
+    await StoreContextSeed.SeedAsync(context);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+	throw;
+}
 
 app.Run();
---
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -0,0 +1,37 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class StoreContextSeed
+    {
+        //creating static class for Seeding data
+        public static async Task SeedAsync(StoreContext context)
+        {
+            if (!context.Products.Any())
+            {
+                //Read Json file
+                var productsData = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/products.json");
+
+                //Deserializing json data ==> C# objects
+                var products = JsonSerializer.Deserialize<List<Product>>(productsData);
+
+                //check for product is null or not
+                if (products == null)
+                {
+                    return;
+                }
+
+                //use AddRange to add multiple entities to table
+                context.Products.AddRange(products);
+
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
